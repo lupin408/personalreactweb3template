8d8f7cdedcc59a5b8dc1d0b81d611305f44a15bd
Eric Sanchirico <eccisketchi@gmail.com>
2022-01-13 16:43:49 -0500

    modernoutline

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
 import logo from './logo.svg';
 import './App.css';
+import Web3 from 'web3';
+import React from 'react';
+import abi from './abicode.js'
 
-function App() {
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+     reloadnum: 0
+    };
+    this.getAccount = this.getAccount.bind(this);
+    this.injectweb3andcontract = this.injectweb3andcontract.bind(this);
+  }
+ 
+  
+  componentDidMount(b) {
+    
+    if (typeof window.ethereum !== 'undefined') {
+      console.log('MetaMask is installed!');
+    }
+
+ 
+ 
+}
+  
+injectweb3andcontract(c) {
+  if (window.web3 == undefined) {
+
+  } else {
+  async function loadWeb3() {
+    if (window.ethereum) {
+        window.web3 = new Web3(window.ethereum);
+        window.ethereum.enable();
+    }
+  }
+  
+  async function load() {
+    await loadWeb3();
+    window.contract = await loadContract();
+   
+  }
+  
+  
+  var abi2 =  JSON.stringify(abi.abi)
+  console.log(abi2, typeof abi2)
+  
+  async function loadContract() {
+  
+  return await new window.web3.eth.Contract(JSON.parse(abi2), '0xe97999a2Da794bfE703B2FB053B59f16b920Ec8A');
+  
+  } 
+  load();
+  }
+}
+
+ async getAccount(b){
+  
+  const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+const account = accounts[0];
+console.log(account);
+console.log(window.ethereum)
+if (window.ethereum.chainId !== '0x38'){
+  try {
+    await window.ethereum.request({
+      method: 'wallet_switchEthereumChain',
+      params: [{ chainId: '0x38' }],
+    });
+  } catch (switchError) {
+    // This error code indicates that the chain has not been added to MetaMask.
+    if (switchError.code === 4902) {
+      try {
+        await window.ethereum.request({
+          method: 'wallet_addEthereumChain',
+          params: [{ chainId: '0x38', chainName: 'Smart Chain', rpcUrls: ['https://bsc-dataseed.binance.org/'] /* ... */ }],
+        });
+      } catch (addError) {
+        // handle "add" error
+      }
+    }
+    // handle other "switch" errors
+  }
+  
+}
+this.injectweb3andcontract()
+
+}
+  
+ 
+ 
+ 
+
+
+
+
+  
+
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {window.ethereum == undefined ?    <div>Please install metamask</div> : <button id='connectmetamaskbtn' onClick={this.getAccount}>Connect Metamask to BSC</button> }
+
+     
+ 
+
+     
     </div>
+    
   );
-}
+}}
 
 export default App;
---
+++ src/abicode.js
@@ -0,0 +1,185 @@
+const abi =  [
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "directrefsmap",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "earnedBnb",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "entrantnum",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "entrantsmap",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "indirectrefsmap",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "entrant",
+				"type": "address"
+			}
+		],
+		"name": "otherEntries",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "referrerlist",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "totalearned",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalpyramidval",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "withdrawbnb",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+];
+  module.exports = {
+      abi
+    };
+
+
+  
